f0796efd67c21c534bcd9800461c70a816024496
Nicole Richardson <njrichardson95@gmail.com>
2018-10-11 13:21:53 -0400

    trying to get on click functionality to work

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Main from "./components/Main/Main";
 import Navbar from "./components/Nav/Navbar";
 import Characters from "./characters.json";
 
+function shuffleCharacters(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
 
 class App extends React.Component {
-
-
 
     // Setting this.state.friends to the friends json array
     state = {
@@ -16,6 +21,18 @@ class App extends React.Component {
   
    // Map over this.state.friends and render a FriendCard component for each friend object
 
+   handleShuffle = () => {
+    let shuffledCharacters = shuffleCharacters(Characters);
+    this.setState({ Characters: shuffledCharacters });
+  };
+
+   handleClick = id => {
+    if (this.state.clicked.indexOf(id)) {
+      alert("clicked");
+    } else {
+      this.handleShuffle();
+    }
+  };
 
 
 
@@ -33,6 +50,8 @@ class App extends React.Component {
               id={Character.id}
               name={Character.name}
               image={Character.image}
+              handleClick={this.handleClick}
+              handleShuffle={this.handleShuffle}
             />
           ))}
         </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,11 @@ import React from "react";
 import "./Main.css";
 
 const Main = props => (
-  <div className="cardPic">
+  <div className="cardPic"
+  onClick={() => props.handleClick(props.id)}>
+  
     <div className="img-container z-depth-3">
-      <img alt={props.name} src={props.image} />
+      <img alt={props.name} src={props.image} value={props.id} />
     </div>
     
     {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
